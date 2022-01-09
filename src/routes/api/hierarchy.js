1b9d395492024cab8375f38957182a37d232e3a1
johannchopin <johannchopin@pm.me>
2022-01-09 15:29:58 +0100

    💩 api example

--- src/routes/api/hierarchy.js
+++ src/routes/api/hierarchy.js
@@ -1,6 +1,3 @@
-import { getDirs } from "$lib/utils";
-import * as path from "path";
-
-export async function get() {
-  return getDirs(path.resolve(__dirname, 'docs'))
+export const get = async () => {
+  return {}
 }
