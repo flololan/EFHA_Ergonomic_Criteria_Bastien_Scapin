770778ff6b0ba9179b57cebd0bf7cea3740e448a
johannchopin <johannchopin@pm.me>
2022-01-12 11:52:21 +0100

    ✨ dynamic page title

--- src/routes/[lang]/index.svelte
+++ src/routes/[lang]/index.svelte
@@ -1,6 +1,13 @@
 <script lang="ts">
     import { _ } from 'svelte-i18n'
+
+    $: website = window.location.hostname
+    $: pageTitle = $_('site_title', { values: { website } })
 </script>
+
+<svelte:head>
+  <title>{pageTitle}</title>
+</svelte:head>
 
 <main id="mainContent">
   <h1>Blat</h1>
