4c08ba24c4ca9109f03fb7fa4a716fb770e1de3a
Lyokolux <lyokolux@protonmail.com>
2022-01-12 11:20:23 +0100

    feat(generate-index.ts): add next and previous link to an entry

--- src/scripts/generate-index.ts
+++ src/scripts/generate-index.ts
@@ -1,58 +1,102 @@
 import fs from 'fs'
 import path from 'path'
 
+/*
+ * Quick script to generate an index used for the navigation
+ */
+
 const SRC_DIR = 'static/contents'
 const DST_FILE = 'static/content-index.json'
 
+type EntrySlug = string;
 export type IndexEntry = {
-    key: string
-    lang: 'fr' | 'en'
-    path: string
+    slug: EntrySlug
+    resource: string
     children?: IndexEntry[]
+    next?: EntrySlug
+    previous?: EntrySlug
 }
 
-const getAllFiles = function(dirPath: string, arrayOfFiles: string[] = []) {
+const getAllDirectories = function(dirPath: string, arrayOfDirectories: string[] = []) {
     const files = fs.readdirSync(dirPath)
     
-    files.forEach(function(file) {
-      if (fs.statSync(path.join(dirPath, "/", file)).isDirectory()) {
-        getAllFiles(path.join(dirPath, "/", file), arrayOfFiles)
-      } else {
-        arrayOfFiles.push(path.join(dirPath, "/", file))
-      }
+    files.forEach((file) => {
+        const subpath = path.join(dirPath, "/", file)
+        if (fs.statSync(subpath).isDirectory()) {
+            arrayOfDirectories.push(subpath)
+            getAllDirectories(subpath, arrayOfDirectories)
+        }
     })
-  
-    return arrayOfFiles
+
+    return arrayOfDirectories
+}
+
+const getContentFromPath = (acc: IndexEntry[], pathToFile: string): IndexEntry[] => {
+    const indexEntry = {
+        slug: pathToFile.split(path.sep).slice(2).join(path.sep), 
+        resource: pathToFile,
+    }
+
+    const parentKey = indexEntry.slug.split(path.sep)[0]
+    const isChildOfAtIndex = (parentKey) ? acc.findIndex((entry) => entry.slug === parentKey): undefined
+
+    if (isChildOfAtIndex === -1) {
+        acc.push(indexEntry)
+    } else if (acc[isChildOfAtIndex].children) {
+        acc[isChildOfAtIndex].children.push(indexEntry)
+    } else {
+        acc[isChildOfAtIndex].children = [indexEntry]
+    }
+
+    return acc
+}
+
+const addLinks = (content: IndexEntry[]) => {
+    const addNextLink = (entry: IndexEntry, i: number, isChild = false, entries=content) => {
+        if (entry.children) {
+            entry.next = entry.children[0].slug
+        } else if (isChild) {
+            const children = entries[i].children
+            const childIndex = children.findIndex((e) => e.slug === entry.slug)
+            const currentEntry = children[childIndex]
+            currentEntry.next = (childIndex + 1 < children.length) ? children[childIndex + 1].slug : undefined
+        } else if (i + 1 < entries.length) {
+            entry.next = entries[i+1].slug
+        }
+    }
+
+    const addPreviousLink = (entry: IndexEntry, index: number, isChild = false, entries=content) => {
+        if (isChild) {
+            const children = entries[index].children
+            const childIndex = children.findIndex((e) => e.slug === entry.slug)
+            const currentEntry = children[childIndex]
+            currentEntry.previous = (childIndex - 1 > 0) ? children[childIndex - 1].slug : entries[index].slug
+        } else if (index > 0) {
+            entry.previous = entries[index - 1].slug
+        }
+    }
+
+    content.forEach((entry, i) => {
+        addNextLink(entry, i)
+        addPreviousLink(entry, i)
+
+        // Second level
+        if (entry.children) {
+            entry.children.forEach((entry) => {
+                addNextLink(entry, i, true)
+                addPreviousLink(entry, i, true)
+            })
+        }
+    })
 }
 
 const main = () => {
-    const paths = getAllFiles(SRC_DIR);
-    paths.sort((a, b) => a.length - b.length)
-    
-    const content = paths.reduce((acc, p) => {
-        const parsedPath = path.parse(p)
+    const paths = getAllDirectories(SRC_DIR);
 
-        const indexEntry = {
-            key: parsedPath.dir.split(path.sep).pop(),
-            lang: parsedPath.name as IndexEntry['lang'],
-            path: p,
-        }
+    const content = paths.reduce(getContentFromPath, [] as IndexEntry[])
+    addLinks(content) // in-place operation
 
-        const parentKey = parsedPath.dir.split(path.sep).reverse()[1]
-        const isChildOfAtIndex = acc.findIndex((entry) => entry.key === parentKey)
-
-        if (isChildOfAtIndex === -1) {
-            acc.push(indexEntry)
-        } else if (acc[isChildOfAtIndex].children) {
-            acc[isChildOfAtIndex].children.push(indexEntry)
-        } else {
-            acc[isChildOfAtIndex].children = [indexEntry]
-        }
-
-        return acc
-    }, [] as IndexEntry[])
-
-    fs.writeFileSync(DST_FILE, JSON.stringify(content))    
+    fs.writeFileSync(DST_FILE, JSON.stringify(content, undefined, 2))    
 }
 
 main()
