185c62a581b781cb09cca2e777472a6b7789e179
johannchopin <johannchopin@pm.me>
2022-01-12 11:53:05 +0100

    🚧 Dynamic title for doc page

--- src/routes/[lang]/docs/[...filename]/index.svelte
+++ src/routes/[lang]/docs/[...filename]/index.svelte
@@ -18,11 +18,18 @@
 </script>
 
 <script>
+  import { _ } from 'svelte-i18n'
   import { getMdAsHtml } from "$lib/mdToHtmlParser"
 
   export let md
 
+  $: website = window.location.hostname
+  $: pageTitle = $_('site_title', { values: { website } })
   let html = getMdAsHtml(md, { pageSlug: location.pathname })
 </script>
+
+<svelte:head>
+  <title>{pageTitle}</title>
+</svelte:head>
 
 {@html html}
