aafba62ab5260ce5f834cd5480efbfc3fbc5cf78
Lyokolux <lyokolux@protonmail.com>
2022-01-14 00:02:31 +0100

    ✨(EditOnGithub.svelte): add basic button - wip 2

--- src/lib/components/EditOnGithub.svelte
+++ src/lib/components/EditOnGithub.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
     import { EDIT_CRITERIA_ROOT_PATH } from "$lib/const";
-    import { locale } from "svelte-i18n";
+    import { locale, _ } from "svelte-i18n";
 
     export let slug: string;
 
@@ -9,14 +9,14 @@
 </script>
 
 <a
-    class="d-flex align-items-center position-absolute top-0 end-0 mt-4 me-4 btn btn-outline-dark"
+    class="d-flex align-items-center position-absolute top-0 end-0 mt-4 me-4 btn btn-secondary"
     href="{EDIT_CRITERIA_ROOT_PATH}/{slug}/{lang}.md"
 >
-    Edit this criteria
+    {$_("edit_this_criteria")}
     <img
         class="github-logo ps-1"
         src="/assets/githubLogo.svg"
-        alt="on github"
+        alt={$_("on_github")}
     />
 </a>
 
@@ -27,6 +27,6 @@
 
     a:hover .github-logo {
         filter: invert(100%);
-        transition: filter;
+        transition: filter 0.15s ease-in-out;
     }
 </style>
--- src/lib/const.ts
+++ src/lib/const.ts
@@ -6,4 +6,4 @@ export const LANGUAGES = {
 export const TITLE = 'is-ergonomic'
 
 export const EMAIL_REGEX = /[^@\s]+@[^@\s]+\.[^@\s]+/
-export const EDIT_CRITERIA_ROOT_PATH = 'https://github.com/flololan/EFHA_Ergonomic_Criteria_Bastien_Scapin/edit/main/docs'
+export const EDIT_CRITERIA_ROOT_PATH = 'https://github.com/flololan/EFHA_Ergonomic_Criteria_Bastien_Scapin/edit/main/static/contents'
