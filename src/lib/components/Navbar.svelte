ef1e33068698df78b2666657e51b08d414b3894d
Lyokolux <lyokolux@protonmail.com>
2022-01-15 14:48:42 +0100

    Revert "♻️(Navbar.svelte): use $app/paths for SSR"
    This reverts commit 911b54b1ba1845f883cb1f25b44d5febaeb391ff.

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,38 +1,14 @@
 <script lang="ts">
-	import { page } from '$app/stores';
-	import { base } from '$app/paths';
-	import { _ } from 'svelte-i18n';
-	import { TITLE } from '$lib/const';
+  import { page } from '$app/stores'
+  import { _ } from 'svelte-i18n'
+  import { TITLE } from '$lib/const'
 
-	import LocalisationSelect from './LocalisationSelect.svelte';
-	import Links from './Links.svelte';
+  import LocalisationSelect from "./LocalisationSelect.svelte";
+  import Links from "./Links.svelte";
 
-	$: path = $page.url.pathname;
-	$: logoAlt = $_('icon_alt', {
-		values: { website: base },
-	});
+  $: path = $page.url.pathname
+  $: logoAlt = $_('icon_alt', { values: { website: window.location.hostname }})
 </script>
-
-<nav
-	class="navbar navbar-expand-lg px-md-4 pt-0 pb-0 navbar-light bg-light shadow"
->
-	<div class="container-fluid">
-		<a
-			id="brand"
-			rel="prefetch"
-			class="p-0 d-flex align-contents-center link-dark"
-			aria-label={logoAlt}
-			href="/"
-		>
-			<img src="/assets/icon.svg" class="p-2" alt={`${base} page logo`} />
-			<h1 class="d-none d-md-block ms-1 mb-0 fs-4">{TITLE}</h1>
-		</a>
-		<ul class="d-flex ms-auto mb-0">
-			<Links {path} />
-		</ul>
-		<LocalisationSelect />
-	</div>
-</nav>
 
 <style lang="scss">
 	.navbar {
@@ -66,3 +42,17 @@
 		color: black;
 	}
 </style>
+
+
+<nav class="navbar navbar-expand-lg px-md-4 pt-0 pb-0 navbar-light bg-light shadow">
+  <div class="container-fluid">
+    <a id="brand" rel="prefetch" class="p-0 d-flex align-contents-center link-dark" aria-label={logoAlt} href="/">
+			<img src="/assets/icon.svg" class="p-2" alt={`${window.location.hostname} page logo`}>
+			<h1 class="d-none d-md-block ms-1 mb-0 fs-4">{TITLE}</h1>
+    </a>
+    <ul class="d-flex ms-auto mb-0">
+      <Links {path} />
+    </ul>
+    <LocalisationSelect />
+  </div>
+</nav>
