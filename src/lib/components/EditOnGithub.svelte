c36b030da60332d4bb9aadfd9e98ea709dc46482
Lyokolux <lyokolux@protonmail.com>
2022-01-15 10:03:09 +0100

    ♻️(EditOnGithub.svelte): svg direct import as component version

--- src/lib/components/EditOnGithub.svelte
+++ src/lib/components/EditOnGithub.svelte
@@ -1,23 +1,22 @@
 <script lang="ts">
-    import { EDIT_CRITERIA_ROOT_PATH } from "$lib/const";
-    import { locale, _ } from "svelte-i18n";
+    import { EDIT_CRITERIA_ROOT_PATH } from '$lib/const';
+    import GithubLogo from '$lib/svg/GithubLogo.svelte';
+    import { locale, _ } from 'svelte-i18n';
 
     export let slug: string;
 
     let lang;
     $: lang = $locale;
+    $: href = `${EDIT_CRITERIA_ROOT_PATH}/${slug}/${lang}.md`;
 </script>
 
 <a
-    class="d-flex align-items-center position-absolute top-0 end-0 mt-4 me-4 btn btn-secondary"
-    href="{EDIT_CRITERIA_ROOT_PATH}/{slug}/{lang}.md"
+    class="d-flex align-items-center position-absolute top-0 end-0 mt-4 me-4 btn btn-outline-dark"
+    {href}
+    target="_blank"
 >
-    {$_("edit_this_criteria")}
-    <img
-        class="github-logo ps-1"
-        src="/assets/githubLogo.svg"
-        alt={$_("on_github")}
-    />
+    {$_('edit_this_criteria')}
+    <span class="github-logo ms-1"><GithubLogo alt={$_('on_github')} /></span>
 </a>
 
 <style>
@@ -26,7 +25,6 @@
     }
 
     a:hover .github-logo {
-        filter: invert(100%);
         transition: filter 0.15s ease-in-out;
     }
 </style>
