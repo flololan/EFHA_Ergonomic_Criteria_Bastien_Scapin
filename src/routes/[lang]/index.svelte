285831031807dc75bf54b66b9708b4f0bb8171f5
Lyokolux <lyokolux@protonmail.com>
2022-01-16 14:11:50 +0100

    ✨(index.svelte): title is website name

--- src/routes/[lang]/index.svelte
+++ src/routes/[lang]/index.svelte
@@ -1,8 +1,13 @@
 <script lang="ts">
-    import { _ } from 'svelte-i18n'
+  import { _ } from 'svelte-i18n'
+  import { TITLE } from '$lib/const'
 
-    import Footer from '$lib/components/Footer.svelte';
+  import Footer from '$lib/components/Footer.svelte'
 </script>
+
+<svelte:head>
+  <title>{TITLE}</title>
+</svelte:head>
 
 <h1>Blat</h1>
 
