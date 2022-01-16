d8ab36027bde07a39b4dd5b1223d1be79cb8da0c
Lyokolux <lyokolux@protonmail.com>
2022-01-16 11:28:01 +0100

    🚨(__layout.svelte)

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,18 +1,19 @@
 <script context="module" lang="ts">
-  import { waitLocale } from 'svelte-i18n';
+  import { waitLocale } from 'svelte-i18n'
   import '../i18n'
 
   export async function load(): Promise<Record<string, never>> {
     await waitLocale()
+
     return {}
   }
 </script>
 
 <script lang="ts">
-  import GoToMainContent from '$lib/components/GoToMainContent.svelte';
+  import GoToMainContent from '$lib/components/GoToMainContent.svelte'
   import { screen } from '$lib/stores'
 
-  import "../styles/global.scss";
+  import '../styles/global.scss'
 
   let windowHeight: number
   let windowWidth: number
@@ -21,15 +22,12 @@
     screen.update(() => ({
       height: windowHeight,
       width: windowWidth,
-      device: windowWidth >= 768 ? 'computer' : 'mobile'
+      device: windowWidth >= 768 ? 'computer' : 'mobile',
     }))
   }
 </script>
 
-<svelte:window 
-  bind:innerHeight={windowHeight}
-  bind:innerWidth={windowWidth}
-/>
+<svelte:window bind:innerHeight={windowHeight} bind:innerWidth={windowWidth} />
 
 <GoToMainContent />
 <slot />
