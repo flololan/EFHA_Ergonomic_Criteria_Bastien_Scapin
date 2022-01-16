21a39ebc21294eca1edd7b29b48624deab42384e
Lyokolux <lyokolux@protonmail.com>
2022-01-16 10:48:59 +0100

    🐛(index.svelte): i18n key

--- src/routes/[lang]/criteria/[...filename]/index.svelte
+++ src/routes/[lang]/criteria/[...filename]/index.svelte
@@ -1,99 +1,97 @@
 <script context="module" lang="ts">
-    import type { Lang, NavStructure } from '$lib/type';
+  import type { Lang, NavStructure } from '$lib/type'
 
-    export const getArticleUrl = (slug: string, lang: Lang): string =>
-        `/contents/${slug}/${lang}.md`;
+  export const getArticleUrl = (slug: string, lang: Lang): string =>
+    `/contents/${slug}/${lang}.md`
 
-    export async function load({ params, fetch, stuff }) {
-        const res = await fetch(getArticleUrl(params.filename, params.lang));
-        if (res.ok) {
-            return {
-                props: {
-                    md: await res.text(),
-                    slug: params.filename,
-                    nav: stuff.nav,
-                },
-            };
-        }
-
-        return {
-            status: res.status,
-            error: new Error(
-                `The documentation for /${params.filename} doesn't exist!`
-            ),
-        };
+  export async function load({ params, fetch, stuff }) {
+    const res = await fetch(getArticleUrl(params.filename, params.lang))
+    if (res.ok) {
+      return {
+        props: {
+          md: await res.text(),
+          slug: params.filename,
+          nav: stuff.nav,
+        },
+      }
     }
+
+    return {
+      status: res.status,
+      error: new Error(
+        `The documentation for /${params.filename} doesn't exist!`
+      ),
+    }
+  }
 </script>
 
 <script lang="ts">
-    import { getMdAsHtml } from '$lib/mdToHtmlParser';
-    import { _, locale } from 'svelte-i18n';
-    import { DEFAULT_LANG } from '$lib/const';
-    import type { NavItem } from '$lib/type';
-    import EditOnGithub from '$lib/components/EditOnGithub.svelte';
+  import { getMdAsHtml } from '$lib/mdToHtmlParser'
+  import { _, locale } from 'svelte-i18n'
+  import { DEFAULT_LANG } from '$lib/const'
+  import type { NavItem } from '$lib/type'
+  import EditOnGithub from '$lib/components/EditOnGithub.svelte'
 
-    export let md: string;
-    export let slug: string;
-    export let nav: NavStructure;
+  export let md: string
+  export let slug: string
+  export let nav: NavStructure
 
-    const getCurrentNavItem = (
-        slug: NavItem['slug'],
-        navItems: NavItem[]
-    ): NavItem | undefined => {
-        for (let i = 0; i < navItems.length; i++) {
-            const item = navItems[i];
-            if (slug === item.slug) {
-                return item;
-            }
-            if (item.children) {
-                const matchingNavItem = getCurrentNavItem(slug, item.children);
-                if (matchingNavItem) {
-                    return matchingNavItem;
-                }
-            }
+  const getCurrentNavItem = (
+    slug: NavItem['slug'],
+    navItems: NavItem[]
+  ): NavItem | undefined => {
+    for (let i = 0; i < navItems.length; i++) {
+      const item = navItems[i]
+      if (slug === item.slug) {
+        return item
+      }
+      if (item.children) {
+        const matchingNavItem = getCurrentNavItem(slug, item.children)
+        if (matchingNavItem) {
+          return matchingNavItem
         }
-    };
+      }
+    }
+  }
 
-    const getCriteriaUrl = (slug) => `/${lang}/criteria/${slug}`;
+  const getCriteriaUrl = (slug) => `/${lang}/criteria/${slug}`
 
-    $: lang = ($locale as Lang | null | undefined) ?? DEFAULT_LANG;
-    $: html = getMdAsHtml(md, { pageSlug: location.pathname });
+  $: lang = ($locale as Lang | null | undefined) ?? DEFAULT_LANG
+  $: html = getMdAsHtml(md, { pageSlug: location.pathname })
 
-    $: currentNavItem = getCurrentNavItem(slug, nav.children);
-    $: prev = currentNavItem?.previous;
-    $: next = currentNavItem?.next;
+  $: currentNavItem = getCurrentNavItem(slug, nav.children)
+  $: prev = currentNavItem?.previous
+  $: next = currentNavItem?.next
 </script>
 
 <article id="mainContent" class="criteria position-relative px-5 pt-4">
-    {@html html}
-    <EditOnGithub {slug} />
-    <div class="row d-flex justify-content-between my-5">
-        {#if prev}
-            <a
-                class="btn btn-outline-secondary btn-sm col-9 col-md-5"
-                href={getCriteriaUrl(prev)}
-            >
-                <span class="me-1" aria-hidden="true">←</span>
-                {$_('previous_criteria')}
-            </a>
-        {/if}
-        {#if next}
-            <a
-                class="btn btn-outline-secondary btn-sm col-9 col-md-5 ms-auto mt-2 mt-md-0"
-                href={getCriteriaUrl(next)}
-            >
-                {$_('next_criteria')}<span class="ms-1" aria-hidden="true">
-                    →</span
-                >
-            </a>
-        {/if}
-    </div>
+  {@html html}
+  <EditOnGithub {slug} />
+  <div class="row d-flex justify-content-between my-5">
+    {#if prev}
+      <a
+        class="btn btn-outline-secondary btn-sm col-9 col-md-5"
+        href={getCriteriaUrl(prev)}
+      >
+        <span class="me-1" aria-hidden="true">←</span>
+        {$_('previous_criterion')}
+      </a>
+    {/if}
+    {#if next}
+      <a
+        class="btn btn-outline-secondary btn-sm col-9 col-md-5 ms-auto mt-2 mt-md-0"
+        href={getCriteriaUrl(next)}
+      >
+        {$_('next_criterion')}<span class="ms-1" aria-hidden="true"> →</span>
+      </a>
+    {/if}
+  </div>
 </article>
 
 <style>
-    article {
-        width: 80ch;
-        max-width: 100%;
-        text-align: justify;
-    }
+  article {
+    width: 80ch;
+    max-width: 100%;
+    text-align: justify;
+  }
 </style>
