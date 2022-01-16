72b2a7243353456dd193ac39a33dbc18a0824974
Lyokolux <lyokolux@protonmail.com>
2022-01-16 14:12:56 +0100

    ✨(lang/criteria.svelte): add criteria title

--- src/routes/[lang]/criteria/index.svelte
+++ src/routes/[lang]/criteria/index.svelte
@@ -1,1 +1,10 @@
+<script lang="ts">
+  import { wrapTitle } from '$lib/utils'
+  import { _ } from 'svelte-i18n'
+</script>
+
+<svelte:head>
+  <title>{wrapTitle($_('criteria'))}</title>
+</svelte:head>
+
 <h1>Welcome to the doc</h1>
