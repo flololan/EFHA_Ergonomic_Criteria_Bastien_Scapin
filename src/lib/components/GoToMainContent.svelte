613e6725987e71e27b857f5828ddf73b842749c4
Lyokolux <lyokolux@protonmail.com>
2022-01-18 19:43:06 +0100

    💄(GoToMainContent.svelte): link color constrast is improved

--- src/lib/components/GoToMainContent.svelte
+++ src/lib/components/GoToMainContent.svelte
@@ -2,12 +2,15 @@
   import { _ } from 'svelte-i18n'
 </script>
 
-<style>
+<a
+  href="#mainContent"
+  class="position-absolute top-0 start-0 btn btn-outline-secondary bg-light text-primary m-2 visually-hidden-focusable"
+>
+  {$_('goto_main_content')}
+</a>
+
+<style lang="scss">
   a {
     z-index: 5;
   }
 </style>
-
-<a href="#mainContent" class="position-absolute top-0 start-0 btn btn-outline-secondary bg-light m-2 visually-hidden-focusable">
-  {$_('goto_main_content')}
-</a>
